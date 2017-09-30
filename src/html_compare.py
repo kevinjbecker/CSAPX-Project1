bb536172333e128af10d6d705f72952b2cd30dfa
Kevin <kjb2503@rit.edu>
2017-09-30 14:17:51 -0400

    Added functions that will be needed to be implemented

---
+++ src/html_compare.py
@@ -0,0 +1,15 @@
+
+def main():
+    pass
+
+
+def html_split_rec(html_contents_file_1: str, html_contents_file_2: str)->str:
+    pass
+
+
+def compare_strings(html_string_1: str, htlm_string_2: str)->str:
+    pass
+
+
+if __name__ == '__main__':
+    main()
