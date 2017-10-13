2fa2864af98127a4779a0600f85a34059a87c53b
Kevin <kjb2503@rit.edu>
2017-10-12 21:10:38 -0400

    Implemented a better management of the comparing

--- src/html_compare.py
+++ src/html_compare.py
@@ -2,8 +2,10 @@ from sys import argv
 
 
 def main():
+    # to add: prompt for file names if argv no have them
     file_path_1 = argv[1]
     file_path_2 = argv[2]
+    exit_codes = ["Files match.", "Files partially match with above differences.", "Files do not match."]
 
     # if the files are the same path, it is impossible for them to be different.
     # saving compute time by catching this case.
@@ -19,25 +21,35 @@ def main():
         else:
             file_1_stripped = strip_new_lines(file_1_html)
             file_1_line_preserved = replace_new_lines(file_1_html)
-            file_1_line_count = 0
             file_2_stripped = strip_new_lines(file_2_html)
             file_2_line_preserved = replace_new_lines(file_2_html)
-            file_2_line_count = 0
 
-            continue_at_index = -1
+            # EXIT CODE KEY: 0 = MATCH, 1 = PARTIAL MATCH, 2 = NO MATCH
+            exit_code = compare_files(file_1_stripped, file_1_line_preserved, file_2_stripped, file_2_line_preserved)
 
-            for index in range(0, len(file_1_stripped)):
-                print(file_1_stripped[index] + " / " + file_2_stripped[index])
-                if index >= len(file_2_stripped):
-                    print("ERROR: Unexpected end of file 2. Aborting operation.")
-                    break
-                if file_1_stripped[index] != file_2_stripped[index]:
-                    error = find_error(file_1_stripped, file_2_stripped, file_1_line_preserved, file_2_line_preserved, index)
-                    if error:
-                        break
+            print(exit_codes[exit_code])
 
 
-def find_error(file_1: str, file_2: str, file_1_lines: str, file_2_lines: str, start_index: int)->bool:
+def compare_files(file_1_stripped: str, file_1_line_preserved: str, file_2_stripped: str, file_2_line_preserved: str)->int:
+    if file_1_stripped == file_2_stripped:
+        return 0
+
+    file_1_stripped_copy = file_1_stripped
+    file_2_stripped_copy = file_2_stripped
+
+    index = 0
+    while len(file_1_stripped) > 0:
+        index += 1
+        if len(file_2_stripped) == 0:
+            print("Unexpected end of file 2... aborting operation.\nFiles do not match.")
+            break
+        if file_1_stripped[0] != file_2_stripped[0]:
+            continue_points = find_errors(file_1_stripped_copy, file_2_stripped_copy, file_1_line_preserved,
+                                          file_2_line_preserved, index)
+    return 0
+
+
+def find_errors(file_1: str, file_2: str, file_1_lines: str, file_2_lines: str, start_index: int)->bool:
 
     # gets the nearest start tags for both files
     file_1_start_tag = find_previous_open_tag(file_1, start_index)
@@ -64,12 +76,10 @@ def find_error(file_1: str, file_2: str, file_1_lines: str, file_2_lines: str, s
 
 # still need to calculate number
     if file_1_compare_string != file_2_compare_string:
-        print(["On the following lines...\nfile 1: #. " + file_1[file_1_start_tag[1]: file_1_end_tag[2]]
-                             + "\nfile 2: #." + file_2[file_2_start_tag[1]: file_2_end_tag[2]] + "\n\"" +
-                             file_1_compare_string+"\" != \"" + file_2_compare_string +
-                             "\". Simple text mismatch. Continuing.", True])
-
-    return error_return
+        print("On the following lines...\nfile 1: #. " + file_1[file_1_start_tag[1]: file_1_end_tag[2]]
+              + "\nfile 2: #. " + file_2[file_2_start_tag[1]: file_2_end_tag[2]] + "\n\"" +
+              file_1_compare_string+"\" != \"" + file_2_compare_string +
+              "\". Simple text mismatch. Continuing.")
 
 
 def find_previous_open_tag(file_string: str, start_search_index: int)->list:
@@ -121,11 +131,9 @@ def find_matching_close_tag(file_string: str, start_search_index: int, tag_to_ma
 
 def read_html_file(html_file_path: str)->str:
     html_file_string = ""
-
     with open(html_file_path) as file:
         for line in file:
             html_file_string += line
-
     return html_file_string
 
 
@@ -133,7 +141,6 @@ def strip_white_space(html_string: str, new_lines: bool = False)->str:
     to_return = " ".join(html_string.split()).replace("> <", "><").replace("> ", ">").replace(" <", "<")
     if new_lines:
         to_return = to_return.replace(">$ ", ">")
-
     return to_return
 
 
